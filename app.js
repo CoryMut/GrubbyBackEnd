0cc79e3aebe290c48b5dd8f01e429ef3c6b8bffc
CoryMut <59666828+CoryMut@users.noreply.github.com>
2021-01-15 15:11:39 -0500

    process of moving out of subdomain

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(
     cors({
-        origin: ["https://beta.grubbythegrape.com", "https://www.grubbythegrape.com"],
+        origin: ["https://grubbythegrape.com/", "https://www.grubbythegrape.com"],
         credentials: true,
         exposedHeaders: ["set-cookie"],
     })
