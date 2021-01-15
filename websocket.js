950df73303cb4211455d80963e7d778f6387ce96
CoryMut <59666828+CoryMut@users.noreply.github.com>
2021-01-15 12:06:30 -0500

    potential fix for ws issues

--- websocket.js
+++ websocket.js
@@ -15,9 +15,17 @@ const wss = new WebSocket.Server({
     path: "/comic/upload",
     verifyClient: async function (info, callback) {
         try {
-            console.log(info.req.headers);
+            console.log(info.req.headers.cookie);
+            console.log(info.req.cookies);
+            console.log("-----------------------------");
+            console.log(info.req);
+            console.log("-----------------------------");
             const cookie = info.req.headers.cookie.split("authcookie=")[1];
-            let result = await verifyCookie(cookie);
+            console.log("---cookie---", cookie);
+            const newCookie = info.req.headers.cookie.match(/authcookie=(.*)/)[1].split(";")[0];
+            console.log("---newcookie---", newCookie);
+
+            let result = await verifyCookie(newCookie);
             if (!result) {
                 callback(false, 401, "Unauthorized");
             } else {
