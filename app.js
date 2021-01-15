21908b3a3a57236eb03f9bd5a38149295983a08f
CoryMut <59666828+CoryMut@users.noreply.github.com>
2021-01-15 18:13:52 -0500

    cors problem

--- app.js
+++ app.js
@@ -16,6 +16,12 @@ app.use(
         exposedHeaders: ["set-cookie"],
     })
 );
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://grubbythegrape.com"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", true);
+    next();
+});
 
 app.use(fileUpload());
 app.use(cookieParser());
